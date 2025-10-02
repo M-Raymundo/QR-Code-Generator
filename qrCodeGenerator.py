bb797e33d432d66cbc14d24c6ab3d2f2134d08a9
mat <92649949+ifwhileDev@users.noreply.github.com>
2025-10-02 16:03:32 -0300

    Add files via upload

---
+++ qrCodeGenerator.py
@@ -0,0 +1,20 @@
+import qrcode
+from secrets import token_hex
+
+token = "URL"
+
+def qrCodeGenerate():
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(token)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save('qrcode.png')
+
+qrCodeGenerate()
+
